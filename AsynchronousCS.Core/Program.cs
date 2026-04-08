85d23a7d0a09b13c7255158d793f257b06bcdb00
Khalos CF Moscato <khalos.moscato@gmail.com>
2026-04-08 11:42:41 +0100

    feat: complete task 2

--- AsynchronousCS.Core/Program.cs
+++ AsynchronousCS.Core/Program.cs
@@ -1,10 +1,29 @@
-﻿namespace AsynchronousCS.Core
+﻿using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+
+namespace AsynchronousCS.Core
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            var printHello = Task.Run(async () =>
+            {
+                Console.WriteLine("Hello..");
+                await Task.Delay(3000);
+            });
+
+            var printWorld = Task.Run(async () => 
+            {
+                Console.WriteLine(".. World!");
+                await Task.Delay(3000);
+            });
+
+            await Task.WhenAll([printHello, printWorld]);
+            stopwatch.Stop();
         }
     }
 }
